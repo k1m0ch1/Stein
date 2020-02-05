48f107114d07403b0f112c72bde3454e8935e03e
Dimas <dimasjkl@gmail.com>
2020-02-05 18:20:26 +0700

    feat(objectDoesMatch): provide mongo-style comparison search operators

--- controllers/objectDoesMatch.js
+++ controllers/objectDoesMatch.js
@@ -1,12 +1,61 @@
 // Compares an object to another
 
+const OPERATORS_COMPARISON = [
+  '$eq',
+  '$gt',
+  '$gte',
+  '$lt',
+  '$lte',
+  '$ne',
+];
+
 module.exports = (query, obj) => {
   let match = true;
   for (let key in query) {
-    if (obj[key] !== query[key]) {
-      match = false;
-      break;
+    const criteria = query[key];
+    if (typeof criteria === 'object') {
+      match = compareCriteria(criteria, obj[key]);
+      if (!match) break;
+    } else {
+      if (obj[key] !== query[key]) {
+        match = false;
+        break;
+      }
     }
   }
   return match;
 };
+
+const compareCriteria = (criteria, test) => {
+  let match = true;
+  for (const [ operator, value ] of Object.entries(criteria)) {
+    match = compareValue(operator, value, test);
+    if (!match) break;
+  }
+  return match;
+}
+
+const compareValue = (operator, value, test) => {
+  if (!OPERATORS_COMPARISON.includes(operator)) {
+    return false;
+  }
+  if (operator === '$eq' && test === value) {
+    return true;
+  }
+  if (operator === '$gt' && test > value) {
+    return true;
+  }
+  if (operator === '$gte' && test >= value) {
+    return true;
+  }
+  if (operator === '$lt' && test < value) {
+    return true;
+  }
+  if (operator === '$lte' && test <= value) {
+    return true;
+  }
+  if (operator === '$ne' && test !== value) {
+    return true;
+  }
+  return false;
+}
